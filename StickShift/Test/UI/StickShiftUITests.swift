c72ebf2baedffd9a6f7b4e17ace76e67e66c082b
Joshua Moody <joshuajmoody@gmail.com>
2017-12-19 16:59:48 +0100

    Test: correct test methods namespaces
    And remove unnecessary ;

--- StickShift/Test/UI/StickShiftUITests.swift
+++ StickShift/Test/UI/StickShiftUITests.swift
@@ -13,27 +13,29 @@ class StickShiftUITests: XCTestCase {
         super.tearDown()
     }
 
-    func testMCLaunchUsingSwift() {
+    func testACTLaunchUsingSwift() {
         let app = ACTLaunch.launch()
         XCTAssertNotNil(app)
         ACTLabel.labelStep("Given the app launched using ACTLaunch.launch from Swift")
         app?.terminate()
     }
 
-    func testMCLaunchApplicationUsingSwift() {
-        let app = XCUIApplication();
-        let launched = ACTLaunch.launch(app);
+    func testACTLaunchApplicationUsingSwift() {
+        let app = XCUIApplication()
+        let launched = ACTLaunch.launch(app)
         XCTAssertEqual(app, launched,
                        "Expected .launch(app): to return the application it" +
-            "passed as an argument.");
-        ACTLabel.labelStep("Given app launched using ACTLaunch.launch(app) from Swift");
-        app.terminate();
+            "was passed as an argument.")
+        ACTLabel.labelStep("Given app launched using ACTLaunch.launch(app) from Swift")
+        app.terminate()
     }
 
-    func testMCLabelClassMethodWithSwift() {
+    func testACTLabelClassMethodWithSwift() {
+        let app: XCUIApplication = ACTLaunch.launch()
         ACTLabel.labelStep("Given the app has launched")
         ACTLabel.labelStep("Then I %@ the %@ button %@ times",
                            args: getVaList(["touch", "red", "3"]))
-        XCTAssert(true, "This test should always pass");
+        XCTAssert(true, "This test should always pass")
+        app.terminate()
     }
 }
